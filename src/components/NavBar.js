7fb06bc4fa3ab16a19679b8bf281de60582a3d2a
MMendes24 <mmbusiness1@protonmail.com>
2020-11-05 10:41:50 -0600

    Router set up in index.js

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,9 +1,24 @@
 import React from "react"
-import AppStyles from "../styling"
+import { Link } from 'react-router-dom'
+import AppStyles from "../styling/AppStyles"
 
 const NavBar = () => {
     return(
-        <div></div>
+        <nav>
+            <Link>Test</Link>
+            <Link>Test</Link>
+            <a className="social-link" href="https://github.com/MMendes24">
+                <i class="fab fa-github-square"></i>
+            </a>
+
+            <a className="social-link" href="https://twitter.com/MarsMendes2">
+                <i class="fab fa-twitter-square" ></i>
+            </a>
+
+            <a className="social-link" href="https://www.linkedin.com/in/marsdmendes/">
+                <i class="fab fa-linkedin"></i>
+            </a>
+        </nav>
     )
 }
 
